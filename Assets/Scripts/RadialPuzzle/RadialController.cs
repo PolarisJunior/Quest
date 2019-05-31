3db95065d6b6c13a79ce161b556973939df98362
PolarisJunior <brownasianpj@yahoo.com>
2019-05-30 21:01:54 -0700

    radial controller

--- Assets/Scripts/RadialPuzzle/RadialController.cs
+++ Assets/Scripts/RadialPuzzle/RadialController.cs
@@ -1,88 +1,90 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// // @AJSTEAMVR @COMMENTEDOUT
+// @AJSTEAMVR @COMMENTEDOUT
 
-// namespace Valve.VR.InteractionSystem
-// {
-//     public class RadialController : MonoBehaviour
-//     {
+namespace Valve.VR.InteractionSystem
+{
+    public class RadialController : MonoBehaviour
+    {
 
-//         public float radius = 10f;
+        public float radius = 10f;
 
-//         public float heightOffset = 0f;
+        public float heightOffset = 0f;
 
-//         public GameObject netObj;
+        public GameObject netObj;
 
-//         public GameObject leftNetObj;
+        public GameObject leftNetObj;
 
-//         public Hand rightHand;
+        // public Hand rightHand;
 
-//         public Hand leftHand;
-
-
-//         public Transform originTransform;
-
-//         private bool mouseMode = true;
-
-//         // Start is called before the first frame update
-//         void Start()
-//         {
-//             mouseMode = rightHand == null && leftHand == null;
-//         }
-
-//         void Update()
-//         {
-//             // we have to use world positions since this is not necessarily a child of originTransform
-//             if (mouseMode)
-//             {
-//                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//                 ray.direction = new Vector3(ray.direction.x, 0, ray.direction.z).normalized;
-//                 // this doesn't really make sense unless it originTransform is the camera
-//                 ray.origin = originTransform.position;
-//                 Vector3 pos = ray.origin + (ray.direction * radius);
-
-//                 netObj.transform.position = new Vector3(pos.x, originTransform.position.y + heightOffset, pos.z);
-//             }
-//             else
-//             {
-//                 {
-//                     Vector3 transformPos = originTransform.position;
-//                     Vector3 dir = (rightHand.transform.forward).normalized;
-
-//                     Ray ray = new Ray();
-//                     ray.origin = transformPos;
-//                     ray.direction = new Vector3(dir.x, 0, dir.z).normalized;
+        // public Hand leftHand;
+        public GameObject rightHand;
+        public GameObject leftHand;
 
 
-//                     Vector3 pos = ray.origin + (ray.direction * radius);
-//                     // netObj.transform.position = new Vector3(pos.x, transform.position.y + heightOffset, pos.z);
-//                     netObj.transform.position = new Vector3(pos.x, rightHand.transform.position.y + heightOffset, pos.z);
-//                 }
-//                 {
-//                     Vector3 transformPos = originTransform.position;
-//                     Vector3 dir = (leftHand.transform.forward).normalized;
+        public Transform originTransform;
 
-//                     Ray ray = new Ray();
-//                     ray.origin = transformPos;
-//                     ray.direction = new Vector3(dir.x, 0, dir.z).normalized;
+        private bool mouseMode = true;
 
-//                     Vector3 pos = ray.origin + (ray.direction * radius);
-//                     // netObj.transform.position = new Vector3(pos.x, transform.position.y + heightOffset, pos.z);
-//                     leftNetObj.transform.position = new Vector3(pos.x, leftHand.transform.position.y + heightOffset, pos.z);
-//                 }
-//             }
-//         }
+        // Start is called before the first frame update
+        void Start()
+        {
+            mouseMode = rightHand == null && leftHand == null;
+        }
 
-//         void OnEnable()
-//         {
+        void Update()
+        {
+            // we have to use world positions since this is not necessarily a child of originTransform
+            if (mouseMode)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                ray.direction = new Vector3(ray.direction.x, 0, ray.direction.z).normalized;
+                // this doesn't really make sense unless it originTransform is the camera
+                ray.origin = originTransform.position;
+                Vector3 pos = ray.origin + (ray.direction * radius);
 
-//         }
+                netObj.transform.position = new Vector3(pos.x, originTransform.position.y + heightOffset, pos.z);
+            }
+            else
+            {
+                {
+                    Vector3 transformPos = originTransform.position;
+                    Vector3 dir = (rightHand.transform.forward).normalized;
 
-//         void OnDisable()
-//         {
+                    Ray ray = new Ray();
+                    ray.origin = transformPos;
+                    ray.direction = new Vector3(dir.x, 0, dir.z).normalized;
 
-//         }
-//     }
-// }
+
+                    Vector3 pos = ray.origin + (ray.direction * radius);
+                    // netObj.transform.position = new Vector3(pos.x, transform.position.y + heightOffset, pos.z);
+                    netObj.transform.position = new Vector3(pos.x, rightHand.transform.position.y + heightOffset, pos.z);
+                }
+                {
+                    Vector3 transformPos = originTransform.position;
+                    Vector3 dir = (leftHand.transform.forward).normalized;
+
+                    Ray ray = new Ray();
+                    ray.origin = transformPos;
+                    ray.direction = new Vector3(dir.x, 0, dir.z).normalized;
+
+                    Vector3 pos = ray.origin + (ray.direction * radius);
+                    // netObj.transform.position = new Vector3(pos.x, transform.position.y + heightOffset, pos.z);
+                    leftNetObj.transform.position = new Vector3(pos.x, leftHand.transform.position.y + heightOffset, pos.z);
+                }
+            }
+        }
+
+        void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+
+        }
+    }
+}
